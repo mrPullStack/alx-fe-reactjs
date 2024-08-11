d074569ef24e07cf84400405b89034bb1b2238d7
SuNgu <cliffland.jasungu@gmail.com>
2024-08-11 18:27:02 +0300

    create userprofile component and display user information using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import './App.css'
 
@@ -14,6 +15,7 @@ function App() {
           <WelcomeMessage />
           <Header />
           <MainContent />
+          <UserProfile name="Alice" age="25" bio="loves hiking and photography" />
           <Footer />
       </div>
       
