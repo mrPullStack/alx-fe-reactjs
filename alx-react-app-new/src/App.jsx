e6ec2de31f8d5b7c296320759b6c21d05dbf41a0
SuNgu <cliffland.jasungu@gmail.com>
2024-08-18 22:28:41 +0300

    implemented counter functionality using useState hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,6 +5,8 @@ import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import './App.css'
+import React from 'react'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +19,7 @@ function App() {
           <MainContent />
           <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
           <Footer />
+          <Counter />
       </div>
       
     </>
