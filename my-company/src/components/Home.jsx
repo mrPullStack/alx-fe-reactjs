379b902fc503c8022cd217981a1915f7438a20d8
SuNgu <cliffland.jasungu@gmail.com>
2024-08-24 16:36:28 +0300

    added home.jsx

--- my-company/src/components/Home.jsx
+++ my-company/src/components/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
             <h1>Welcome to Our Company</h1>
             <p>We are dedicated to delivering excellence in all our services.</p>
         </div>
-    );
+        );
 }
 
 export default Home;
