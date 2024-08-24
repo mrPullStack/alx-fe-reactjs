c5e55e4c8a13a7ba900c4c342a91564e7e25deb8
SuNgu <cliffland.jasungu@gmail.com>
2024-08-24 17:36:16 +0300

    added navbar

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 function App(){
   return (
