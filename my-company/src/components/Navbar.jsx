b7317a2df8e91cbdc1f02addb363934426db4933
SuNgu <cliffland.jasungu@gmail.com>
2024-08-24 17:47:58 +0300

    added inline style to navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar(){
+    const navbarStyle = {
+        backgroundColor: '#333',
+        color: '#fff',
+        display: 'flex',
+        justifyContent: 'space-around',
+        padding: '10px',
+    };
+
+    const linkStyle = {
+        color: '#fff',
+        textDecoration: 'none',
+    }
     return (
         <nav style={{ padding: '10px', backgroundColor: '#eee', textAlign: 'center' }}>
             <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
