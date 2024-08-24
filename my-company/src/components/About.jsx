c9ab9a37fcc1c0ee391b7fb4ff5fa16a4ba3dc2e
SuNgu <cliffland.jasungu@gmail.com>
2024-08-24 16:41:15 +0300

    added contact.jsx

--- my-company/src/components/About.jsx
+++ my-company/src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
             <h1>About Us</h1>
             <p>Our company has been providing top-notch services since 1990. We specialize in various fields including technology, marketing, and consultancy.</p>
         </div>
-        );
+    );
 }
 
 export default About;
