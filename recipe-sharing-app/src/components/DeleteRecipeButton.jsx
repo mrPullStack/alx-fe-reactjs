ea0b8082a8107cb22c908d27720e269628479710
SuNgu <cliffland.jasungu@gmail.com>
2024-09-01 17:58:30 +0300

    added routes to app.jsx

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useRecipeStore from '../recipeStore';
+import { useNavigate } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
     const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
