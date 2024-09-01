2f005ffd32e33b5c8b0f9c0bc407b32a9b51ff88
SuNgu <cliffland.jasungu@gmail.com>
2024-09-01 18:37:43 +0300

    integrated favorites and recommendations into the ui

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,6 +4,8 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 const App = () => {
   return (
@@ -16,6 +18,8 @@ const App = () => {
           <Route path="/" element={<RecipeList />} />
           <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
         </Routes>
+        <FavoritesList />
+        <RecommendationsList />
       </div>
     </Router>
   );
