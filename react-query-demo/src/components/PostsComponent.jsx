94f603c2514ccf27404336beab9de62b616fc662
SuNgu <cliffland.jasungu@gmail.com>
2024-09-08 07:05:37 +0300

    added cachetime staletime refetchonwindows focus and keep previous data

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -7,10 +7,15 @@ const fetchPosts = async () => {
         throw new Error('Network response was not ok');
     }
     return response.json();
-    };
+};
 
-    const PostsComponent = () => {
-    const { data, error, isLoading, isError } = useQuery('posts', fetchPosts);
+const PostsComponent = () => {
+    const { data, error, isLoading, isError, refetch, isFetching } = useQuery('posts', fetchPosts, {
+        cacheTime: 1000 * 60 * 10, // Cache time of 10 minutes
+        staleTime: 1000 * 60 * 5,  // Data is fresh for 5 minutes
+        refetchOnWindowFocus: false, // Do not refetch on window focus
+        keepPreviousData: true,      // Keep old data while fetching new data
+    });
 
     if (isLoading) {
         return <div>Loading...</div>;
@@ -24,6 +29,7 @@ const fetchPosts = async () => {
         <div>
         <h1>Posts</h1>
         <button onClick={() => refetch()}>Refetch Posts</button>
+        {isFetching && <p>Updating data...</p>}
         {data.map(post => (
             <div key={post.id}>
             <h2>{post.title}</h2>
