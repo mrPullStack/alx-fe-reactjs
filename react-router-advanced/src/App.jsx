a6d0cf555a458d11e630395db9d7ffb8ab73176d
SuNgu <cliffland.jasungu@gmail.com>
2024-09-08 08:24:15 +0300

    added authentication checker

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,13 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import Blogpost from './components/Blogpost';
+import ProtectedRoute from './assets/components/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
+        <Route 
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuthenticated={isLoggedin}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/posts/:postId" element={<Blogpost />} />
       </Routes>
     </Router>
