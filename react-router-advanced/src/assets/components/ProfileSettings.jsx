c8b9ba745f8780a1b7f248d0243cdada7e67d501
SuNgu <cliffland.jasungu@gmail.com>
2024-09-08 08:17:26 +0300

    added component for the profile setting

---
+++ react-router-advanced/src/assets/components/ProfileSettings.jsx
@@ -0,0 +1,10 @@
+function ProfileSettings() {
+    return (
+        <div>
+            <h3>Profile Settings</h3>
+            <p>Here you can change your profile settings...</p>
+        </div>
+        );
+    }
+
+export default ProfileSettings;
