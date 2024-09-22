83b4442df094ece1d2b9b666eae54f0462c3105a
SuNgu <cliffland.jasungu@gmail.com>
2024-09-22 19:53:35 +0300

    updated githubservice

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,19 +2,17 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com';
 
-// Function to fetch user data from GitHub API
-export const fetchUserData = async (username) => {
-    const { username, location, repos } = queryParams;
-
-    // Construct the query string for advanced search
+// Function to fetch GitHub users with advanced search parameters
+export const fetchUserData = async ({ username = '', location = '', minRepos = 0 }) => {
+    // Construct the search query for the API request
     let query = `q=${username ? username : ''}`;
     
     if (location) {
         query += `+location:${location}`;
     }
     
-    if (repos) {
-        query += `+repos:${minRepos}`;
+    if (minRepos > 0) {
+        query += `+repos:>=${minRepos}`;
     }
 
     try {
