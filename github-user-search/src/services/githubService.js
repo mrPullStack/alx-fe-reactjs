9908dbc10f2dded81b544571ccf461f61c3549c4
SuNgu <cliffland.jasungu@gmail.com>
2024-09-22 16:39:35 +0300

    added githubservice

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const GITHUB_API_URL = 'https://api.github.com/users/';
+
+// Function to search GitHub users by username
+export const searchGitHubUsers = async (username) => {
+    try {
+        const response = await axios.get(`${GITHUB_API_URL}${username}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching GitHub users:', error);
+        throw error;
+    }
+};
